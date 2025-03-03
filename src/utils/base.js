fb64baa61fd2528984c5685c55763c0273c2302f
blackcave0 <i3intel@protonmail.com>
2025-03-03 06:57:51 +0530

    correcting the base url

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:8080";
 export const BASE_URL =
-  "https://matrimonial-server-hhoj9dzcv-sarvesh-s-projects-66e932e0.vercel.app/hello-daksh";
+  "https://matrimonial-server-hhoj9dzcv-sarvesh-s-projects-66e932e0.vercel.app";
