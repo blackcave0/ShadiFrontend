71796abef7674dbe3740bf53fc5812ff99c0f673
blackcave0 <i3intel@protonmail.com>
2025-03-03 07:14:00 +0530

    removing the proxy from the package.json and changing the base url

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,3 +1,2 @@
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL =
-  "https://matrimonial-server-hhoj9dzcv-sarvesh-s-projects-66e932e0.vercel.app";
+export const BASE_URL = "https://true-matrimonial.onrender.com";
