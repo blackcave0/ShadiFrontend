7dd9f8ac8967bae95b083c60caffb3f41b5b49dc
blackcave0 <i3intel@protonmail.com>
2025-03-04 23:56:29 +0530

    changing the backend server url

--- src/utils/base.js
+++ src/utils/base.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://true-matrimonial.onrender.com";
+// export const BASE_URL = "https://true-matrimonial.onrender.com";
+export const BASE_URL = "https://matrimonial-server-two.vercel.app/";
